e46cf4b966ae6fd07eae1e9c21766c7f9f5b9187
nealian <nealian@163.com>
2020-03-21 13:15:47 +0800

    添加剪裁和水印

--- TextToImage.py
+++ TextToImage.py
@@ -1,6 +1,8 @@
+import numpy
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
 import random
+import math
 
 
 def subsection(string):
@@ -13,16 +15,6 @@ def carry(x, y):
     return x // y
 
 
-def insert_poster(sections, poster, num=3):
-    if num > len(sections):
-        num = len(sections)
-    poster_pos = [random.randint(0, len(sections)) for _ in range(num)]
-    for i in range(len(sections)):
-        if i in poster_pos:
-            sections[i] = sections[i] + poster
-    return sections
-
-
 def linefeed(sections, length):
     stringlins = []
     for section in sections:
@@ -31,19 +23,79 @@ def linefeed(sections, length):
     return stringlins
 
 
-def text2lins(string, poster, length):
-    segments = subsection(string);
-    segments = insert_poster(segments, poster)
+def text2lins(string, length):
+    segments = subsection(string)
     string_lins = linefeed(segments, length)
     return string_lins
 
 
-def text2piiic(string, poster, length, fontsize=20, x=20, y=40, spacing=20):
-    lins = text2lins(string, poster, length)
-    heigh = y * 2 + (fontsize + spacing) * len(lins)
+def is_Chinese(word):
+    for ch in word:
+        if '\u4e00' <= ch <= '\u9fff':
+            return True
+    return False
+
+
+def add_watermark(image, text, font, alpha=100, space=100):
+    new_img = Image.new(
+        'RGBA', (image.size[0] * 3, image.size[1] * 3), (0, 0, 0, 0))
+    new_img.paste(image, image.size)
+    font_len = len(text)
+    rgba_image = new_img.convert('RGBA')
+    text_overlay = Image.new('RGBA', rgba_image.size, (255, 255, 255, 0))
+    image_draw = ImageDraw.Draw(text_overlay)
+
+    for i in range(0, rgba_image.size[0], font.size*font_len+space):
+        for j in range(0, rgba_image.size[1], font.size+space):
+            image_draw.text((i, j), text, font=font, fill=(0, 0, 0, alpha))
+    text_overlay = text_overlay.rotate(45)
+    image_with_text = Image.alpha_composite(rgba_image, text_overlay)
+
+    image_with_text = image_with_text.crop(
+        (image.size[0], image.size[1], image.size[0] * 2, image.size[1] * 2))
+    return image_with_text
+
+
+def add_single_watermark(image, text, font, alpha=100, position=''):
+    fontsize = font.size
+    new_img = Image.new(
+        'RGBA', image.size, (255, 255, 255, 255))
+    new_img.paste(image)
+    from functools import reduce
+    # 中文占一个宽度，英文占半个宽度
+    width = int(reduce(lambda x, y: x + y,
+                       list(map(lambda x: 1 if is_Chinese(x) else 0.5, text))) * fontsize)
+    picture = Image.new('RGBA', (width, width), (255, 255, 255, 0))
+    draw = ImageDraw.Draw(picture)
+    draw.text((0, (width - fontsize)//2), text,
+              font=font, fill=(0, 0, 0, alpha))
+    x = 0
+    y = 0
+    picture = picture.rotate(45)
+    if position == 'topleft':
+        x = 0
+        y = 0
+    elif position == 'bottomright':
+        x = image.width - width
+        y = image.height - width
+    else:
+        x = (image.width - width) // 2
+        y = (image.height - width) // 2
+    picture2 = Image.new(
+        'RGBA', (image.width, image.height), (255, 255, 255, 0))
+    picture2.paste(picture, (x, y, x+width, y+width))
+    new_img.alpha_composite(picture2)
+    return new_img
+
+
+def text2piiic(string, length, font, x=20, y=40, spacing=20, cut=False):
+    fontsize = font.size
+    lins = text2lins(string, length)
+    heigh = y * 2 + (fontsize + spacing) * (len(lins)-1) + fontsize
     width = x * 2 + fontsize * length
-    font = ImageFont.truetype('./simhei.ttf', fontsize, encoding="utf-8")
-    picture = Image.new('RGB', (width, heigh), (255, 255, 255))
+    if cut:
+        width = x*2 + fontsize * max(list(map(lambda x: len(x), lins)))
+    picture = Image.new('RGBA', (width, heigh), (255, 255, 255, 255))
     draw = ImageDraw.Draw(picture)
     for i in range(len(lins)):
         y_pos = y + i * (fontsize + spacing)
@@ -51,20 +103,21 @@ def text2piiic(string, poster, length, fontsize=20, x=20, y=40, spacing=20):
     return picture
 
 
-def text2multigraph(string, poster, backdrop, fontsize=20, x=20, y=40, spacing=20):
+def text2multigraph(string, backdrop, font, x=20, y=40, spacing=20):
+    fontsize = font.size
     row = (backdrop.width - x * 2) // fontsize
     lin = carry((backdrop.height - y * 2), (fontsize + spacing))
-    str_lin = text2lins(string, poster, row)
+    str_lin = text2lins(string, row)
     str_lin_len = len(str_lin)
     num_lin = carry(str_lin_len, lin)
-    font = ImageFont.truetype('simhei.ttf', fontsize, encoding="utf-8")
     imgs = []
     for num in range(num_lin):
         img = backdrop.copy()
         draw = ImageDraw.Draw(img)
         for i in range(lin):
             if (num * lin + i) < str_lin_len:
-                draw.text((x, y + i * (fontsize + spacing)), str_lin[num * lin + i], font=font, fill=(0, 0, 0))
+                draw.text((x, y + i * (fontsize + spacing)),
+                          str_lin[num * lin + i], font=font, fill=(0, 0, 0))
             else:
                 break
         imgs.append(img)
@@ -75,8 +128,10 @@ if __name__ == "__main__":
     f = open("a.txt")
     str = f.read()
     f.close()
-    imgs = text2multigraph(str, "(公众号：无聊三酌，回复1)", Image.new('RGB', (700, 1000), (255, 255, 255)))
-    for img in imgs:
-        img.show()
-    # img=text2piiic(str,"(公众号：无聊三酌，回复1)",30)
-    # img.show()
+    font = ImageFont.truetype('./simhei.ttf', 20, encoding="utf-8")
+    img = text2piiic(str, 40, font, cut=True)
+    img1 = add_watermark(img, 'hello啊', font=font, alpha=100)
+    img2 = add_single_watermark(img, 'hello啊', font=font, alpha=100)
+    img.show()
+    img1.show()
+    img2.show()
